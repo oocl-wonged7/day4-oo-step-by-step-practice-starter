b4f2780fb12390009f1b9c5b80e4555cfef0b996
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-21 14:30:00 +0800

    refactor: rename constant

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,16 +1,18 @@
 package oo;
 
 public class Student extends Person {
+    private static String STUDENT_INTRODUCTION = " I am a student.";
     private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        String introduction ="My name is " + name + ". I am " + age + " years old. I am a student.";
+        String introduction = super.introduce() + STUDENT_INTRODUCTION;
         if (klass != null) {
-            String classIntrodction = " I am in class " + klass.getNumber() + ".";
+            String classIntrodction = String.format(" I am in class %d.", klass.getNumber());
             introduction += classIntrodction;
         }
         return introduction;
@@ -21,7 +23,7 @@ public class Student extends Person {
     }
 
     public Boolean isIn(Klass klass) {
-        if (this.klass == null){
+        if (this.klass == null) {
             return false;
         }
         return this.klass.equals(klass);
--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,12 +1,14 @@
 package oo;
 
 public class Teacher extends Person {
+    private static String TEACHER_INTRODUCTION = " I am a teacher.";
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return ("My name is " + name + ". I am " + age + " years old. I am a teacher.");
+        String introduction = super.introduce() + TEACHER_INTRODUCTION;
+        return introduction;
     }
 }
