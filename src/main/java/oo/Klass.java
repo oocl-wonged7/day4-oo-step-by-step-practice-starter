c78b95c4ca7998c9991b435b768dd8e5284fbc52
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-21 14:11:27 +0800

    feat: implement klass class

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,23 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Klass klass)) return false;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
 }
